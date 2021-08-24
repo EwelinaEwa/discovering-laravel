a2b37f6bf31152916c78967fa321020c0c4bff6d
EwelinaEwa <ewelina.kosciukiewicz@gmail.com>
2021-08-24 11:22:03 +0200

    Add header, footer and main content to the website

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -1,1 +1,28 @@
-<h1>Hello World!</h1>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Page</title>
+</head>
+<body>
+    <header>
+        <a href="">First Post</a><br>
+        <a href="">Second Post</a><br>
+        <a href="">Third Post</a><br>
+    </header>
+
+    <h1>My blog</h1>
+
+    <p>My blog is about me and me only. Me, me, me. You will learn all about me. Nothing else but me. This is going to be super great. All the content about me. Me, me , me and only me.</p>
+
+    <footer>
+        <p>@Me 2021</p>
+    </footer>
+
+</body>
+</html>
+
+
