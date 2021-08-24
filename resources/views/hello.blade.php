226dbf7f457d4aad6a561e9f6696de9813446c81
EwelinaEwa <ewelina.kosciukiewicz@gmail.com>
2021-08-24 12:07:04 +0200

    Add layout.blade.php and bootstrap

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -1,28 +1,41 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My Page</title>
-</head>
-<body>
-    <header>
-        <a href="">First Post</a><br>
-        <a href="">Second Post</a><br>
-        <a href="">Third Post</a><br>
-    </header>
+@extends('layout')
 
-    <h1>My blog</h1>
+@section('header')
+    <div class="container">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">My Blog</a>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Good articles</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Better articles</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Best articles</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    </div>
+@endsection
 
-    <p>My blog is about me and me only. Me, me, me. You will learn all about me. Nothing else but me. This is going to be super great. All the content about me. Me, me , me and only me.</p>
+@section('content')
+    <div class="container">
+    <h1 class="text-center m-5">My Blog</h1>
 
-    <footer>
-        <p>@Me 2021</p>
-    </footer>
+    <p>My blog is about me and me only. Me, me, me. You will learn all about me. Nothing else but me. This is going to be super great. All the content about me. Me, me , me and only me. My blog is about me and me only. Me, me, me. You will learn all about me. Nothing else but me. This is going to be super great. All the content about me. Me, me , me and only me. My blog is about me and me only. Me, me, me. You will learn all about me. Nothing else but me. This is going to be super great. All the content about me. Me, me , me and only me. My blog is about me and me only. Me, me, me. You will learn all about me. Nothing else but me. This is going to be super great. All the content about me. Me, me , me and only me. My blog is about me and me only. Me, me, me. You will learn all about me. Nothing else but me. This is going to be super great. All the content about me. Me, me , me and only me. My blog is about me and me only. Me, me, me. You will learn all about me. Nothing else but me. This is going to be super great. All the content about me. Me, me , me and only me. My blog is about me and me only. Me, me, me. You will learn all about me. Nothing else but me. This is going to be super great. All the content about me. Me, me , me and only me.</p>
+    </div>
+@endsection
 
-</body>
-</html>
+@section('footer')
+    <div class="container text-center">
+        <p class="text-secondary m-5">@Me 2021</p>
+    </div>
+@endsection
 
 
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', function () {
 
 Route::get('/hello',[\App\Http\Controllers\Controller:: class, 'hello']);
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
